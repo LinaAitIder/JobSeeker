0b057696cae13d9c5294e95fd2c97e19ca137fc2
LinaAitIder <linaitider@gmail.com>
2025-05-10 16:59:19 +0100

    Added ApplicationService to handle API requests for Application component

---
+++ react-ui/src/Components/services/ApplicationService.jsx
@@ -0,0 +1,39 @@
+import api from "../../api/axiosConfig";
+
+class ApplicationService{
+
+    applyToOfferRequest(applicationData){
+        try{
+            return api.post(`/candidature`, applicationData);
+        } catch(err){
+            console.log(err);
+        }
+    }
+
+    getApplicationsByOfferRequest(offerId){
+        try{
+            return api.get(`candidature/offre/${offerId}`);
+        } catch(err){
+             throw new Error(err);
+        }
+    }
+
+    updateApplicationStatusRequest(applicationId){
+        try{
+            return api.patch(`candidature/${applicationId}/status`);
+        } catch(err){
+             throw new Error(err);
+        }
+    }
+
+    deleteApplicationRequest(applicationId){
+        try{
+            return api.delete(`candidature/${applicationId}`);
+        } catch(err){
+             throw new Error(err);
+        }
+    }
+
+}
+
+export default new ApplicationService();
