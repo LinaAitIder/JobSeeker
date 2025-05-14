308d25cc6f214d0b08c0eb1d2653e6a805812d40
LinaAitIder <linaitider@gmail.com>
2025-05-14 20:05:33 +0100

    Enhancing homepage design and responsiveness

--- react-ui/src/Components/authentication/Home.jsx
+++ react-ui/src/Components/authentication/Home.jsx
@@ -2,29 +2,24 @@ import React from 'react';
 import HomeHeader from '../utils/headers/HomeHeader';
 const Home=()=>{
   return (
-      <div className="bg-black h-screen ">
+      <div className="bg-white h-screen ">
         <HomeHeader/>
-        <div className="flex flex-row justify-between h-1/2 w-full bg-black">
-          <div class="flex flex-col bg-black w-3/6 p-3">
-            <span className="text-white text-4xl text-center pt-20 pb-5 font-serif">JobSeeker </span>
-            <div className="pl-10 m-0">
-              <p className="text-white italic">Searching for a Job/Internship in morocco?</p>
-              <p className="text-white italic" >Or searching for a Candidat/Profile?</p>
-              <p className="text-white italic">We are here to help you</p>
+          <div className="flex flex-col bg-white p-3 sm:flex-row place-content-between">
+            <div className="sm:flex-col sm:flex sm:p-3 sm:m-3 sm:opacity-100">
+              <span className="text-black text-4xl text-center pt-10 pb-5 font-serif sm:text-center sm:pt-10 block ">JobSeeker </span>
+              <div className=" m-0 flex flex-col place-content-center align-center">
+                <p className="text-black  text-center ">Searching for a Job or an Internship in Morocco?</p>
+                <p className="text-black text-center " >Or searching for a Candidat?</p>
+                <p className="text-black text-center ">We are here to help you</p>
+              </div>
             </div>
-            <div className="pt-0 pb-0 px-4 m-10">
-              <img src="./assets/searchCandidat.png" className="rounded-full p"/>
+            <div className="py-1 px-16 m-10 lg:w-1/2 ">
+              <img src="./assets/searchCandidat.png" className="rounded-full w-screen"/>
             </div>
           </div>
-          <div className="flex flex-col  w-3/6  m-6 p-8 bg-white justify-start rounded-full ">
-            <img src="./assets/hire.png" className=" rounded-full pt-8"/>
-            <br/>
-            <br/>
-            <br/>
-            <img src="./assets/searchJob.png" className="rounded-full pt-8"/>
-          </div>
+        <hr/>
+
         </div>
-      </div>
   );
 }
 
