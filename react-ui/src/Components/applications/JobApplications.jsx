abb2ae44881cf12544bfc6706e9309451a684e50
LinaAitIder <linaitider@gmail.com>
2025-05-18 07:47:59 +0100

    Adding new page

---
+++ react-ui/src/Components/applications/JobApplications.jsx
@@ -0,0 +1,11 @@
+import RecruiterMainHeader from "../utils/headers/RecruiterMainHeader";
+
+
+export default function JobApplications(){
+
+    return(
+        <div>
+            <RecruiterMainHeader/>
+        </div>
+    );
+}
