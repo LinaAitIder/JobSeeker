e92fc6e4d8e9e86c00d57baa419ba853b3ec4383
LinaAitIder <linaitider@gmail.com>
2025-05-30 10:52:29 +0100

    Sticking the home header

--- react-ui/src/Components/utils/headers/HomeHeader.jsx
+++ react-ui/src/Components/utils/headers/HomeHeader.jsx
@@ -5,19 +5,19 @@ import { ChevronDownIcon } from '@heroicons/react/24/solid'
 
 const HomeHeader = () => {
   return (
-    <header className="p-4 bg-blue-500 ">
+    <header className="p-4 bg-blue-500 top-0 sticky w-full z-50">
       <div className="flex flex-row items-center justify-between">
         <span className="text-white font-serif text-2xl pl-4 hover:cursor-pointer" onClick={()=>{
           window.location.href='/'}}>Job Seeker</span>
        <div className='flex flex-row justify-between items-center'>
        <button className="text-black  bg-white border-0 px-6 py-0 text-sm h-11 font-serif rounded-full " onClick={()=>{window.location.href='/login'}}>Login</button>
-       <div className="ml-2">
-        <Menu> 
+       <div className="ml-2 ">
+        <Menu>
         <MenuButton className="text-black font-serif text-sm bg-white px-5 py-3 rounded-md inline-flex gap-2  ">Space
         <ChevronDownIcon className="size-4 fill-black/60" />
 
         </MenuButton>
-        <MenuItems anchor="bottom end" className=" bg-gray-100 rounded-md shadow-lg p-1 ring-opacity-5 focus:outline-none">
+        <MenuItems anchor="bottom end" className="z-50 bg-white rounded-md shadow-lg p-1 ring-1 ring-black/5 focus:outline-none mt-1">
           <MenuItem className="pr-7 mt-2">
             <button className="group flex w-full items-center gap-2 data-[focus]:bg-gray-400 p-2 rounded-sm text-black font-serif" onClick={()=>{window.location.href='/candidateSignUp'}}>
               <AcademicCapIcon className="text-black size-4 "/> 
